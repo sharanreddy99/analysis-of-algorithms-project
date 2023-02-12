769101ef6ff4b9b053d4fa2bbb1852b01b630022
Sharan Reddy <sharansaikonda99@gmail.com>
2023-02-12 18:46:00 -0500

    update task4 logic

--- Project 1/TASK4.py
+++ Project 1/TASK4.py
@@ -1,4 +1,4 @@
-# Algorithm that finds the max houses to be painted based on the latest available house
+# Algorithm that finds the max houses to be painted based on the earliest endDay of each house
 
 from typing import List
 import heapq
@@ -28,48 +28,52 @@ class NodeObj:
 
 
 def main(n: int, m: int, days: List[int]) -> List[int]:
-    # minHeap contained objects sorted based on shortest duration
-    days = [NodeObj(days[i][0], days[i][1], i) for i in range(m)]
-    heapq.heapify(days)
+    # earliestEndDayHeap consists of intervals that end the earliest
+    earliestEndDayHeap = []
+    heapq.heapify(earliestEndDayHeap)
 
     # startDay indicates the current day
     startDay = 0
+    daysIdx = 0
 
     # An array which stores the indices of houses painted.
     resultIndicesArr: List[int] = []
 
-    while len(days) > 0:
-        node: NodeObj = heapq.heappop(days)
-        if startDay < node.startDay:
-            startDay = node.startDay
+    for startDay in range(1, n + 1):
+        while daysIdx < m and days[daysIdx][0] <= startDay:
+            if days[daysIdx][1] >= startDay:
+                heapq.heappush(
+                    earliestEndDayHeap,
+                    NodeObj(days[daysIdx][0], days[daysIdx][1], daysIdx),
+                )
+            daysIdx += 1
 
-        # if the current house has a endDate less than or equal to current startDate, we paint that house
-        if node.endDay <= startDay:
-            resultIndicesArr.append(node.index)
-            startDay += 1
+        while len(earliestEndDayHeap) > 0:
+            node: NodeObj = heapq.heappop(earliestEndDayHeap)
+            if startDay >= node.startDay and startDay <= node.endDay:
+                resultIndicesArr.append(node.index)
+                break
 
     return resultIndicesArr
 
 
 """
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+TIME COMPLEXITY  : O(n + m*logm)
+SPACE COMPLEXITY : O(n + m) 
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 => Instance where this algorithm yield an optimal answer?
 => When the starting houses have larger duration than the subsequent houses.
-=> n = 5; m = 5; days = [(1,2), (1,3), (1,5), (2, 3), (3, 4)]
-
-Minheap sorted with shortest duration:
-    top -> (1, 2) -> (1, 3) -> (2, 3) -> (3, 4) -> (1,5) 
-    stack = []
+=> n = 5; m = 4; days = [(1,2), (1,3), (1,4), (2, 3)]
 
 According to current algorithm:
 1) House at index 0 is painted on day 1 => 1 lies between (1, 2)
 2) House at index 1 is painted on day 2 => 2 lies between (1, 3)
 3) House at index 3 is painted on day 3 => 3 lies between (2, 3)
-3) House at index 4 is painted on day 4 => 4 lies between (3, 4)
-3) House at index 2 is painted on day 5 => 5 lies between (1, 5)
-Total number of houses painted = 5 (0, 1, 3, 4, 2)
+3) House at index 2 is painted on day 4 => 4 lies between (1, 4)
+Total number of houses painted = 4 (0, 1, 3, 2)
 
 Optimal Solution:
 The above solution is the optimal one as it paints all the houses available
@@ -78,4 +82,5 @@ The above solution is the optimal one as it paints all the houses available
 
 => Instance where this algorithm doesn't yield an optimal answer?
     This greedy approach always yields the optimal solution
+        
 """
