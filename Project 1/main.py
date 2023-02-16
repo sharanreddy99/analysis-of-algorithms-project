2a799fa04aba1da9e009a4c7591338cd6ae2da7d
Sharan Reddy <sharansaikonda99@gmail.com>
2023-02-16 00:20:55 -0500

    read from stdin

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,5 +1,5 @@
 # Given array is already sorted by startDay and to sorted by endDay to break a tie
-from helpers import generateDummyInput, prepareFunctionCall, displayOutput
+from helpers import generateDummyInput, prepareFunctionCall, displayOutput, readInput
 
 import TASK1
 import TASK2
@@ -9,7 +9,7 @@ import TASK5
 
 
 if __name__ == "__main__":
-    n, m, days = generateDummyInput()
+    n, m, days = readInput()
     funcCall = prepareFunctionCall()
     res = []
     if len(days) > 0:
