a09ad61859203ec9f4932adeca80371acb139585
Sharan Reddy <sharansaikonda99@gmail.com>
2023-02-16 10:54:28 -0500

    refactor code

--- Project 1/helpers/helpers.py
+++ Project 1/helpers/helpers.py
@@ -15,11 +15,39 @@ def prepareFunctionCall():
 
 
 def compareOptimalTasksWithMultipleInput():
-    fp = open("output.json", "w")
-    args = sys.argv
-    for i in range(100):
-        n, m, days = generateDummyInput(int(args[4]), int(args[5]))
-        days.sort()
+    with open("output.json", "w") as fp:
+        args = sys.argv
+        for i in range(int(args[5])):
+            n, m, days = generateDummyInput(int(args[3]), int(args[4]))
+            days.sort()
+            fp.write(
+                "Input: n ({0}), m: ({1}), days: ${2}\n".format(n, m, json.dumps(days))
+            )
+
+            tasks = list(map(int, args[2].split(",")))
+            resArr = []
+            for task in tasks:
+                res = eval("TASK{0}.main(n, m, days)".format(task))
+                resArr.append(res)
+                fp.write("Result of task{0}: {1}\n".format(task, json.dumps(res)))
+
+            for i in range(len(tasks)):
+                for j in range(i + 1, len(tasks)):
+                    fp.write(
+                        "task{0} length: {1}, task{2} length: {3}\n".format(
+                            tasks[i], len(resArr[i]), tasks[j], len(resArr[j])
+                        )
+                    )
+
+            fp.write("\n\n")
+
+
+def compareOptimalTasksWithSingleInput():
+    with open("output_single.json", "w") as fp:
+        args = sys.argv
+        n, m, days = readDummyInput()
+        if len(args) == 6:
+            n, m, days = generateDummyInput(int(args[4]), int(args[5]))
         fp.write(
             "Comparision between task-{0} and task{1}\n_____________________________\n".format(
                 int(args[2]), int(args[3])
@@ -32,29 +60,11 @@ def compareOptimalTasksWithMultipleInput():
         fp.write(json.dumps(res2) + "\n")
         writeIfEqual(fp, res1, res2)
         fp.write("\n\n")
-        fp.close()
-
-
-def compareOptimalTasksWithSingleInput():
-    fp = open("output_single.json", "w")
-    args = sys.argv
-    n, m, days = readDummyInput()
-    fp.write(
-        "Comparision between task-{0} and task{1}\n_____________________________\n".format(
-            int(args[2]), int(args[3])
-        )
-    )
-    fp.write(str(n) + " , " + str(m) + ", " + json.dumps(days) + "\n")
-    res1 = eval("TASK{0}.main(n, m, days)".format(int(args[2])))
-    res2 = eval("TASK{0}.main(n, m, days)".format(int(args[3])))
-    fp.write(json.dumps(res1) + "\n")
-    fp.write(json.dumps(res2) + "\n")
-    writeIfEqual(fp, res1, res2)
-    fp.write("\n\n")
-    fp.close()
 
 
 def writeIfEqual(fp, res1, res2):
+    res1.sort()
+    res2.sort()
     isEqual = False
     if len(res1) == len(res2):
         for i in range(len(res1)):
--- Project 1/helpers/operations.py
+++
@@ -1,6 +0,0 @@
-def init():
-    operation1 = 0
-    operation2 = 0
-    operation3 = 0
-    operation4 = 0
-    operation5 = 0
