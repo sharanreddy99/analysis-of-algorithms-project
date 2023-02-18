5b6b3b84cae22e8a2304e2d81d1744f710fa4e0e
Sharan Reddy <sharansaikonda99@gmail.com>
2023-02-17 21:43:59 -0500

    add support for line chart

--- Project 1/helpers/plothelper.py
+++ Project 1/helpers/plothelper.py
@@ -2,26 +2,36 @@ import json
 import os
 import sys
 from matplotlib import pyplot as plt
-from matplotlib.lines import Line2D
 
 
 def plotDataFromOutputFile():
-    yaxisAttributeName = sys.argv[2]
+    xaxisAttributeName = sys.argv[2]
+    yaxisAttributeName = sys.argv[3]
+    xLabel = sys.argv[4]
+    yLabel = sys.argv[5]
+    filename = sys.argv[6]
+    combinedDataKey = "combinedData"
     dataArr = {
-        i: {"n": [], "m": [], "respLength": [], "executionTime": []}
+        i: {
+            "n": [],
+            "m": [],
+            combinedDataKey: [],
+            "respLength": [],
+            "executionTime": [],
+        }
         for i in range(1, 5)
     }
 
     dirList = os.listdir("./output")
-    # attributesMap = {
-    #     1: {"color": "black", "alpha": 1, "edgecolor": "yellow"},
-    #     2: {"color": "yellow", "alpha": 1, "edgecolor": "blue"},
-    #     3: {"color": "red", "alpha": 0.6, "edgecolor": "green"},
-    #     4: {"color": "blue", "alpha": 0.5, "edgecolor": "yellow"},
-    # }
-    # for key in dataArr:
-    #     for attribute in attributesMap[key]:
-    #         dataArr[key][attribute] = attributesMap[key][attribute]
+    attributesMap = {
+        1: {"borderStyle": "m--"},
+        2: {"borderStyle": "r-."},
+        3: {"borderStyle": "k-o"},
+        4: {"borderStyle": "c-*"},
+    }
+    for key in dataArr:
+        for attribute in attributesMap[key]:
+            dataArr[key][attribute] = attributesMap[key][attribute]
 
     for fileName in dirList:
         with open("./output/" + fileName, "r") as fp:
@@ -37,65 +47,32 @@ def plotDataFromOutputFile():
                     dataArr[data["task"]][key].append(data[key])
                     dataArr[data["task"]]["label"] = "TASK - {0}".format(data["task"])
 
+    for i in range(1, 5):
+        dataArr[i][combinedDataKey] = list(
+            zip(dataArr[i][xaxisAttributeName], dataArr[i][yaxisAttributeName])
+        )
+        dataArr[i][combinedDataKey].sort(key=lambda x: x[0])
+
     plotDataArr = []
 
-    # No of houses painted
     for key in dataArr:
         plotDataArr.append(
             {
-                "data": dataArr[key][yaxisAttributeName],
+                "xData": [row[0] for row in dataArr[key][combinedDataKey]],
+                "yData": [row[1] for row in dataArr[key][combinedDataKey]],
                 "label": dataArr[key]["label"],
+                "borderStyle": dataArr[key]["borderStyle"],
             }
         )
 
-    plotHistogramStep(plotDataArr)
+    plotLineGraph(plotDataArr, xLabel, yLabel, filename)
 
 
-def plotHistogram(dataArr):
+def plotLineGraph(dataArr, xLabel, yLabel, filename):
     for row in dataArr:
-        plt.hist(
-            row["data"],
-            bins=60,
-            alpha=row["alpha"],
-            label=row["label"],
-            color=row["color"],
-            edgecolor=row["edgecolor"],
-        )
+        plt.plot(row["xData"], row["yData"], row["borderStyle"], label=row["label"])
 
+    plt.xlabel(xLabel, labelpad=5)
+    plt.ylabel(yLabel, labelpad=5)
     plt.legend(loc="upper right")
-    plt.show()
-
-
-def plotHistogramStep(dataArr):
-    fig, ax = plt.subplots(figsize=(9, 5))
-    ax.hist(
-        [dataArr[i]["data"] for i in range(len(dataArr))],
-        bins=60,
-        histtype="step",
-        linewidth=2,
-        alpha=0.7,
-        label=[dataArr[i]["label"] for i in range(len(dataArr))],
-    )
-
-    handles, labels = ax.get_legend_handles_labels()
-    leg_entries = {}
-    for h, label in zip(handles, labels):
-        leg_entries[label] = Line2D(
-            [0],
-            [0],
-            color=h.get_facecolor()[:-1],
-            alpha=h.get_alpha(),
-            lw=h.get_linewidth(),
-        )
-
-    labels_sorted, lines = zip(*sorted(leg_entries.items()))
-    ax.legend(lines, labels_sorted, frameon=False)
-
-    # Remove spines
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-
-    plt.xlabel("AVAILABILITY OF PAINTER", labelpad=15)
-    plt.ylabel("NO OF HOUSES PAINTED", labelpad=15)
-    plt.legend(loc="upper right")
-    plt.show()
+    plt.savefig(filename)
--- Project 1/script.sh
+++ Project 1/script.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-minVal=1
-maxVal=1000
-testCases=20
-tasks=1,2,3,4
-highest=10000
-step=1000
+# minVal=1
+# maxVal=1000
+# testCases=1
+# tasks=1,2,3,4
+# highest=10000
+# step=500
 
-i=1
-n=$((highest / step))
+# i=1
+# n=$((highest / step))
 
-rm input/* output/*
-for ((i = 1; i <= n; i++)); do
-    make rungentest minval=$minVal maxval=$maxVal testcases=$testCases
-    inpFileName=$(find ./input -printf '%T+ %p\n' | sort -r | head -n1 | awk '{print $2}' | awk -F '/' '{print $3}')
-    make runfromtestfile tasks=$tasks filename=$inpFileName
-    ((maxVal += step))
-done
-make runplotoutput yaxis=respLength
+# rm input/* output/*
+# for ((i = 1; i <= n; i++)); do
+#     make rungentest minval=$minVal maxval=$maxVal testcases=$testCases
+#     inpFileName=$(find ./input -printf '%T+ %p\n' | sort -r | head -n1 | awk '{print $2}' | awk -F '/' '{print $3}')
+#     make runfromtestfile tasks=$tasks filename=$inpFileName
+#     ((minVal += step))
+#     ((maxVal += step))
+# done
+make runplotoutput xaxis=n yaxis=m xlabel="AVAILABILITY_OF_PAINTER" ylabel="NO_OF_HOUSES" filename="./plots/Plot2/n_and_m.png"
+make runplotoutput xaxis=n yaxis=respLength xlabel="AVAILABILITY_OF_PAINTER" ylabel="NO_OF_HOUSES_PAINTED" filename="./plots/Plot2/n_and_respLength.png"
+make runplotoutput xaxis=n yaxis=executionTime xlabel="AVAILABILITY_OF_PAINTER" ylabel="EXECUTION_TIME" filename="./plots/Plot2/n_and_executionTime.png"
