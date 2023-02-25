21131800862f291ad19abb8e92b5a3650863270a
Sharan Reddy <sharansaikonda99@gmail.com>
2023-02-25 12:46:05 -0500

    update singlec script

--- Project 1/scriptsingle.sh
+++ Project 1/scriptsingle.sh
@@ -2,13 +2,13 @@
 
 plotName=testplot
 outputRespFolder="./plots/$plotName/"
-inpFileName=testcases_temp.txt
+inpFileName=testcases_mouglano.txt
 tasks=1,2,3,4
 
-make runfromtestfile tasks=$tasks filename=$inpFileName
-make runplotoutput xaxis=n yaxis=m xlabel="AVAILABILITY_OF_PAINTER" ylabel="NO_OF_HOUSES" filename="n_and_m.png" foldername=${outputRespFolder}
-make runplotoutput xaxis=n yaxis=respLength xlabel="AVAILABILITY_OF_PAINTER" ylabel="NO_OF_HOUSES_PAINTED" filename="n_and_respLength.png" foldername=${outputRespFolder}
-make runplotoutput xaxis=n yaxis=executionTime xlabel="AVAILABILITY_OF_PAINTER" ylabel="EXECUTION_TIME" filename="n_and_executionTime.png" foldername=${outputRespFolder}
-make runplotoutput xaxis=m yaxis=executionTime xlabel="NO_OF_HOUSES" ylabel="EXECUTION_TIME" filename="m_and_executionTime.png" foldername=${outputRespFolder}
-make runplotoutput xaxis=m yaxis=respLength xlabel="NO_OF_HOUSES" ylabel="NO_OF_HOUSES_PAINTED" filename="m_and_respLength.png" foldername=${outputRespFolder}
-make rungentable filename="table.html" foldername=${outputRespFolder}
+# make runfromtestfile tasks=$tasks filename=$inpFileName
+make runplotoutput xaxis=n yaxis=m xlabel="AVAILABILITY_OF_PAINTER" ylabel="NO_OF_HOUSES" filename="n_and_m.png" foldername=${outputRespFolder} tasks=${tasks}
+make runplotoutput xaxis=n yaxis=respLength xlabel="AVAILABILITY_OF_PAINTER" ylabel="NO_OF_HOUSES_PAINTED" filename="n_and_respLength.png" foldername=${outputRespFolder} tasks=${tasks}
+make runplotoutput xaxis=n yaxis=executionTime xlabel="AVAILABILITY_OF_PAINTER" ylabel="EXECUTION_TIME" filename="n_and_executionTime.png" foldername=${outputRespFolder} tasks=${tasks}
+make runplotoutput xaxis=m yaxis=executionTime xlabel="NO_OF_HOUSES" ylabel="EXECUTION_TIME" filename="m_and_executionTime.png" foldername=${outputRespFolder} tasks=${tasks}
+make runplotoutput xaxis=m yaxis=respLength xlabel="NO_OF_HOUSES" ylabel="NO_OF_HOUSES_PAINTED" filename="m_and_respLength.png" foldername=${outputRespFolder} tasks=${tasks}
+make rungentable filename="table.html" foldername=${outputRespFolder} tasks=${tasks}
