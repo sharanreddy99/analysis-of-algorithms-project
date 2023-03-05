4f6994e36707aa0defc19ce6f705920cc9de2825
Sharan Reddy <sharansaikonda99@gmail.com>
2023-03-05 10:12:30 -0500

    update task2 logic with slightly unoptimized answer

--- Project 2/TASK2.py
+++ Project 2/TASK2.py
@@ -18,53 +18,50 @@ class Main:
         # Size of the maximal square plot
         self.maxSquareLen = 0
 
+        self.initDPArray()
+
+    def initDPArray(self):
+        # Stores the count of square plots less than h.
+        self.dp = [0 for i in range(self.n)]
+
     def main(self):
         # The first two loops fix the upper left corner for the given square plot.
         for rowStart in range(self.m):
             for colStart in range(self.n):
-                # validPlotsCount stores the number of valid individual plots in the chosen square plots.
-                validPlotsCount = 0
+                # Clear the previous memorized values
+                for x in range(self.n):
+                    self.dp[x] = 0
 
-                # The next two loops fix the lower right corner for the square plot whose upper left corner is fixed and count the plots which satisfy the requirement.
-                rowEnd = rowStart
-                colEnd = colStart
+                # The next two loops fix the lower right corner for the square plot whose upper left corner is fixed and compute the no of mintree plots
+                # in the given square based on the precomputed value from the previous row.
+                for rowEnd in range(rowStart, self.m):
+                    for colEnd in range(colStart, self.n):
+                        self.dp[rowEnd][colEnd] += (
+                            1 if self.p[rowEnd][colEnd] >= self.h else 0
+                        )
 
-                while rowEnd < self.m and colEnd < self.n:
-                    # We check the rightMost column alongside previously chosen square plot.
-                    for row in range(rowStart, rowEnd):
-                        if self.p[row][colEnd] >= self.h:
-                            validPlotsCount += 1
+                        totRows = rowEnd - rowStart + 1
+                        totCols = colEnd - colStart + 1
 
-                    # We check the bottomMost row alongside previously chosen square plot.
-                    for col in range(colStart, colEnd + 1):
-                        if self.p[rowEnd][col] >= self.h:
-                            validPlotsCount += 1
-
-                    totRows = rowEnd - rowStart + 1
-                    totCols = colEnd - colStart + 1
-
-                    # We check whether the chosen plot is an optimal square plot satisfying the min tree requirement and store it if required.
-                    if (
-                        totRows == totCols
-                        and validPlotsCount == totRows * totCols
-                        and totRows > self.maxSquareLen
-                    ):
-                        self.resultIndicesArr[0] = rowStart + 1
-                        self.resultIndicesArr[1] = colStart + 1
-                        self.resultIndicesArr[2] = rowEnd + 1
-                        self.resultIndicesArr[3] = colEnd + 1
-                        self.maxSquareLen = totRows
-
-                    rowEnd += 1
-                    colEnd += 1
+                        # We check whether the chosen plot is an optimal square plot satisfying the min tree requirement and store it if required.
+                        if (
+                            totRows == totCols
+                            and self.dp[rowEnd][colEnd] == totRows * totCols
+                            and totRows > self.maxSquareLen
+                        ):
+                            self.resultIndicesArr[0] = rowStart + 1
+                            self.resultIndicesArr[1] = colStart + 1
+                            self.resultIndicesArr[2] = rowEnd + 1
+                            self.resultIndicesArr[3] = colEnd + 1
+                            self.maxSquareLen = totRows
 
         return self.resultIndicesArr
 
 
 """
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
-TIME COMPLEXITY  : O(m*n*m*n) = O(m^2 * n^2)
-SPACE COMPLEXITY : O(1)
+TIME COMPLEXITY  : O(m*n*m*n)
+SPACE COMPLEXITY : O(n)
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
 """
