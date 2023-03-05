aaae4e607d92d887f8f10222da28b1c807729199
Sharan Reddy <sharansaikonda99@gmail.com>
2023-03-05 15:47:12 -0500

    update task 2

--- Project 2/TASK2.py
+++ Project 2/TASK2.py
@@ -22,21 +22,27 @@ class Main:
 
     def initDPArray(self):
         # Stores the count of plots in a given plot that satisfy the min trees requirement.
-        self.dp = [0 for i in range(self.n)]
+        self.dp = [[0 for i in range(self.n + 1)] for j in range(2)]
 
     def main(self):
         # The first two loops fix the upper left corner for the given square plot.
-        for rowStart in range(self.m):
-            for colStart in range(self.n):
+        for rowStart in range(1, self.m + 1):
+            for colStart in range(1, self.n + 1):
                 # Clear the previous memorized values
-                for x in range(self.n):
-                    self.dp[x] = 0
+                for x in range(2):
+                    for y in range(self.n + 1):
+                        self.dp[x][y] = 0
 
                 # The next two loops fix the lower right corner for the square plot whose upper left corner is fixed and compute the no of mintree plots
                 # in the given square based on the precomputed value from the previous row.
-                for rowEnd in range(rowStart, self.m):
-                    for colEnd in range(colStart, self.n):
-                        self.dp[colEnd] += 1 if self.p[rowEnd][colEnd] >= self.h else 0
+                for rowEnd in range(rowStart, self.m + 1):
+                    for colEnd in range(colStart, self.n + 1):
+                        self.dp[rowEnd % 2][colEnd] = (
+                            self.dp[rowEnd % 2][colEnd - 1]
+                            + self.dp[(rowEnd - 1) % 2][colEnd]
+                            - self.dp[(rowEnd - 1) % 2][colEnd - 1]
+                            + (1 if self.p[rowEnd - 1][colEnd - 1] >= self.h else 0)
+                        )
 
                         totRows = rowEnd - rowStart + 1
                         totCols = colEnd - colStart + 1
@@ -44,13 +50,13 @@ class Main:
                         # We check whether the chosen plot is an optimal square plot satisfying the min tree requirement and store it if required.
                         if (
                             totRows == totCols
-                            and self.dp[colEnd] == totRows * totCols
+                            and self.dp[rowEnd % 2][colEnd] == totRows * totCols
                             and totRows > self.maxSquareLen
                         ):
-                            self.resultIndicesArr[0] = rowStart + 1
-                            self.resultIndicesArr[1] = colStart + 1
-                            self.resultIndicesArr[2] = rowEnd + 1
-                            self.resultIndicesArr[3] = colEnd + 1
+                            self.resultIndicesArr[0] = rowStart
+                            self.resultIndicesArr[1] = colStart
+                            self.resultIndicesArr[2] = rowEnd
+                            self.resultIndicesArr[3] = colEnd
                             self.maxSquareLen = totRows
 
         return self.resultIndicesArr
