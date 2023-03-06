5eb993cb84d35fdbb526a3fc8078eaa589bdb90d
Sharan Reddy <sharansaikonda99@gmail.com>
2023-03-06 15:39:02 -0500

    updated task5A

--- Project 2/TASK5A.py
+++ Project 2/TASK5A.py
@@ -1,7 +1,7 @@
 # Given a matrix p of m ×n integers (non-negative) representing the minimum number of trees that must be planted on each plot and an integer h (positive), find the bounding indices of a square area where all but the corner plots enclosed requires a minimum of h trees to be planted. The corner plots can have any number of trees required
 # Design a Θ(m * n) time Dynamic Programming algorithm for solving Problem2 using Memoization (Top Down)
 class Main:
-    def __init__(self, m, n, h, k, p):
+    def __init__(self, m, n, h, p):
         # No of rows
         self.m = m
 
@@ -13,9 +13,6 @@ class Main:
 
         # Trees planted on each plot
         self.p = p
-
-        # No of exempted square plots
-        self.k = k
 
         # Array which stores the top left and bottom right indices sequentially
         self.resultIndicesArr = [-1, -1, -1, -1]
@@ -36,48 +33,68 @@ class Main:
         self.initDPArray()
 
     def initDPArray(self):
-        # DP[i,j] indicates the largest possible square length having bottom right corner as i,j with atmost k exemptions.
-        self.dp = [[-1 for x in range(self.n + 1)] for y in range(self.m + 1)]
+        # DP[i,j] indicates the largest possible square length having bottom right corner as i,j with corner exemptions.
+        self.dp = [[0 for x in range(self.n + 1)] for y in range(self.m + 1)]
 
-        # invalidCountArr stores the no of plots with minTrees less than the minimum requirement in a given region bounded by top left (0, 0)
+        # validCountArr stores the no of plots satisfying the min tree requirement in the given region bounded by top left (0, 0)
         # and bottom right (i, j)
-        self.invalidCountArr = [
-            [0 for i in range(self.n + 1)] for j in range(self.m + 1)
-        ]
+        self.validCountArr = [[0 for i in range(self.n + 1)] for j in range(self.m + 1)]
 
-        # We compute the invalidCount bounded by [0,0] and [i,j] based on the previously computed values.
+        # We compute the validCount bounded by [0,0] and [i,j] based on the previously computed values.
         for i in range(1, self.m + 1):
             for j in range(1, self.n + 1):
-                self.invalidCountArr[i][j] = (
-                    self.invalidCountArr[i][j - 1]
-                    + self.invalidCountArr[i - 1][j]
-                    - self.invalidCountArr[i - 1][j - 1]
-                    + (1 if self.p[i - 1][j - 1] < self.h else 0)
+                self.validCountArr[i][j] = (
+                    self.validCountArr[i][j - 1]
+                    + self.validCountArr[i - 1][j]
+                    - self.validCountArr[i - 1][j - 1]
+                    + (1 if self.p[i - 1][j - 1] >= self.h else 0)
                 )
 
     # getTopLeft returns the top left corner for a given bottom right position based on the distance provided.
     def getTopLeft(self, rowEnd, colEnd, dist):
         return rowEnd - dist + 1, colEnd - dist + 1
 
-    # getInvalidCount returns the the no of invalid plots in the region bounded by topleft corner [rowStart, colStart] and
+    # getValidCount returns the the no of valid plots in the region bounded by topleft corner [rowStart, colStart] and
     # bottom right corner [rowEnd, colEnd]
-    def getInvalidCount(self, rowStart, colStart, rowEnd, colEnd):
-        invalidCount = self.invalidCountArr[rowEnd][colEnd]
-        invalidCount -= self.invalidCountArr[rowStart - 1][colEnd]
-        invalidCount -= self.invalidCountArr[rowEnd][colStart - 1]
-        invalidCount += self.invalidCountArr[rowStart - 1][colStart - 1]
-        return invalidCount
+    def getValidCount(self, rowStart, colStart, rowEnd, colEnd):
+        validCount = self.validCountArr[rowEnd][colEnd]
+        validCount -= self.validCountArr[rowStart - 1][colEnd]
+        validCount -= self.validCountArr[rowEnd][colStart - 1]
+        validCount += self.validCountArr[rowStart - 1][colStart - 1]
+        return validCount
+
+    # getExemptionCellsCount returns the count of invalid plots from the corners of a given region which can be exempted.
+    def getExemptedCellsCount(self, rowStart, colStart, rowEnd, colEnd):
+        count = 0
+        if self.p[rowStart - 1][colStart - 1] < self.h:
+            count += 1
+
+        if self.p[rowStart - 1][colEnd - 1] < self.h:
+            count += 1
+
+        if self.p[rowEnd - 1][colStart - 1] < self.h:
+            count += 1
+
+        if self.p[rowEnd - 1][colEnd - 1] < self.h:
+            count += 1
+
+        return count
 
     # validateAndStoreRegion checks if the given region satisfies the min trees requirement and updates the result if we get a maximal square region.
     def validateAndStoreRegion(self, rowEnd, colEnd, dist, k):
         rowStart, colStart = self.getTopLeft(rowEnd, colEnd, dist)
         # if the newly found top left corner is a valid position, process the region.
         if rowStart > 0 and colStart > 0 and rowStart <= rowEnd and colStart <= colEnd:
-            invalidCount = self.getInvalidCount(rowStart, colStart, rowEnd, colEnd)
-            if invalidCount <= k:
+            validCount = self.getValidCount(rowStart, colStart, rowEnd, colEnd)
+            exemptedCount = self.getExemptedCellsCount(
+                rowStart, colStart, rowEnd, colEnd
+            )
+
+            totRows = rowEnd - rowStart + 1
+            totCols = colEnd - colStart + 1
+
+            if validCount + exemptedCount >= totRows * totCols:
                 self.dp[rowEnd][colEnd] = max(self.dp[rowEnd][colEnd], dist)
-                totRows = rowEnd - rowStart + 1
-                totCols = colEnd - colStart + 1
 
                 # if the area enclosed by the boundaries forms a square region and is optimal than the previous result, store it.
                 if totRows == totCols and totRows > self.maxSquareLen:
