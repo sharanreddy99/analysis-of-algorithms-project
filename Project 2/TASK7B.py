82b221f654317945fb584b6962fcbcb70ed46f26
Sharan Reddy <sharansaikonda99@gmail.com>
2023-03-06 12:20:14 -0500

    add comments for task7b

--- Project 2/TASK7B.py
+++ Project 2/TASK7B.py
@@ -24,12 +24,16 @@ class Main:
         self.initDPArray()
 
     def initDPArray(self):
+        # intiialize the memorization table with m,n in the x and y dimensions respectively.
         self.dp = [[0 for x in range(self.n + 1)] for y in range(self.m + 1)]
 
+        # invalidCountArr stores the no of plots with minTrees less than the minimum requirement in a given region bounded by top left (0, 0)
+        # and bottom right (i, j)
         self.invalidCountArr = [
             [0 for i in range(self.n + 1)] for j in range(self.m + 1)
         ]
 
+        # We compute the invalidCount bounded by [0,0] and [i,j] based on the previously computed values.
         for i in range(1, self.m + 1):
             for j in range(1, self.n + 1):
                 self.invalidCountArr[i][j] = (
@@ -39,9 +43,12 @@ class Main:
                     + (1 if self.p[i - 1][j - 1] < self.h else 0)
                 )
 
+    # getTopLeft returns the top left corner for a given bottom right position based on the distance provided.
     def getTopLeft(self, rowEnd, colEnd, dist):
         return rowEnd - dist + 1, colEnd - dist + 1
 
+    # getInvalidCount returns the the no of invalid plots in the region bounded by topleft corner [rowStart, colStart] and
+    # bottom right corner [rowEnd, colEnd]
     def getInvalidCount(self, rowStart, colStart, rowEnd, colEnd):
         invalidCount = self.invalidCountArr[rowEnd][colEnd]
         invalidCount -= self.invalidCountArr[rowStart - 1][colEnd]
@@ -49,8 +56,10 @@ class Main:
         invalidCount += self.invalidCountArr[rowStart - 1][colStart - 1]
         return invalidCount
 
-    def computeValidSquare(self, rowEnd, colEnd, dist, k):
+    # validateAndStoreRegion checks if the given region satisfies the min trees requirement and updates the result if we get a maximal square region.
+    def validateAndStoreRegion(self, rowEnd, colEnd, dist, k):
         rowStart, colStart = self.getTopLeft(rowEnd, colEnd, dist)
+        # if the newly found top left corner is a valid position, process the region.
         if rowStart > 0 and colStart > 0:
             invalidCount = self.getInvalidCount(rowStart, colStart, rowEnd, colEnd)
             if invalidCount <= k:
@@ -58,6 +67,7 @@ class Main:
                 totRows = rowEnd - rowStart + 1
                 totCols = colEnd - colStart + 1
 
+                # if the area enclosed by the boundaries forms a square region and is optimal than the previous result, store it.
                 if totRows == totCols and totRows > self.maxSquareLen:
                     self.resultIndicesArr[0] = rowStart
                     self.resultIndicesArr[1] = colStart
@@ -69,14 +79,21 @@ class Main:
         for k in range(self.k + 1):
             for rowEnd in range(1, self.m + 1):
                 for colEnd in range(1, self.n + 1):
+                    # maximum square area formed by the plot which is immediately to top left of the current plot with k - 1 exemptions.
                     dist1 = self.dp[rowEnd - 1][colEnd - 1]
+
+                    # maximum square area formed by the current plot with k - 1 exemptions.
                     dist2 = self.dp[rowEnd][colEnd]
+
+                    # maximum square area formed by the plot which is immediately to left or top of the current plot with k - 1 exemptions and has minimum distance.
                     dist3 = min(
                         self.dp[rowEnd][colEnd - 1], self.dp[rowEnd - 1][colEnd]
                     )
-                    self.computeValidSquare(rowEnd, colEnd, dist1 + 1, k)
-                    self.computeValidSquare(rowEnd, colEnd, dist2 + 1, k)
-                    self.computeValidSquare(rowEnd, colEnd, dist3 + 1, k)
+
+                    # store the optimal area formed by each of the distances obtained above.
+                    self.validateAndStoreRegion(rowEnd, colEnd, dist1 + 1, k)
+                    self.validateAndStoreRegion(rowEnd, colEnd, dist2 + 1, k)
+                    self.validateAndStoreRegion(rowEnd, colEnd, dist3 + 1, k)
 
         return self.resultIndicesArr
 
