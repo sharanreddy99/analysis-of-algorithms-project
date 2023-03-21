c27953a089d5736493fb7f2e25650a1c06ba2305
Sharan Reddy <sharansaikonda99@gmail.com>
2023-03-21 09:58:48 -0400

    fix variable k logic

--- Project 2/helpers/randomlogichelper.py
+++ Project 2/helpers/randomlogichelper.py
@@ -1,16 +1,16 @@
-from random import randint
+import random
 
 
 def task1generator(m, n, h, maxDiff):
-    new_m = randint(2, m)
-    new_n = randint(2, n)
-    new_k = randint(0, max(new_m, new_n))
+    new_m = random.randint(2, m)
+    new_n = random.randint(2, n)
+    new_k = random.randint(0, max(new_m, new_n))
 
     p = [[0 for j in range(new_n)] for i in range(new_m)]
 
     for i in range(new_m):
         for j in range(new_n):
-            p[i][j] = randint(h - maxDiff, h + maxDiff)
+            p[i][j] = random.randint(h - maxDiff, h + maxDiff)
 
     return new_m, new_n, h, new_k, p
 
@@ -18,33 +18,33 @@ def task1generator(m, n, h, maxDiff):
 def taskmngenerator(m, n, h, maxDiff):
     new_m = m
     new_n = n
-    new_k = randint(0, new_m * new_n)
+    new_k = random.randint(0, new_m * new_n)
 
     p = [[0 for j in range(new_n)] for i in range(new_m)]
-
+    customRandom = random.Random(12)
     for i in range(new_m):
         for j in range(new_n):
-            p[i][j] = randint(h - maxDiff, h + maxDiff)
+            p[i][j] = customRandom.randint(h - maxDiff, h + maxDiff)
 
     return new_m, new_n, h, new_k, p
 
 
 def taskmkgenerator(m, n, h, maxDiff):
     new_m = m
-    new_n = randint(0, n + 1)
+    new_n = random.randint(0, n + 1)
     new_k = 4
 
     p = [[0 for j in range(new_n)] for i in range(new_m)]
 
     for i in range(new_m):
         for j in range(new_n):
-            p[i][j] = randint(h - maxDiff, h + maxDiff)
+            p[i][j] = random.randint(h - maxDiff, h + maxDiff)
 
     return new_m, new_n, h, new_k, p
 
 
 def tasknkgenerator(m, n, h, maxDiff):
-    new_m = randint(0, m + 1)
+    new_m = random.randint(0, m + 1)
     new_n = n
     new_k = 4
 
@@ -52,6 +52,6 @@ def tasknkgenerator(m, n, h, maxDiff):
 
     for i in range(new_m):
         for j in range(new_n):
-            p[i][j] = randint(h - maxDiff, h + maxDiff)
+            p[i][j] = random.randint(h - maxDiff, h + maxDiff)
 
     return new_m, new_n, h, new_k, p
